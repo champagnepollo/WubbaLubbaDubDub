7bff4db18c99cbdc9b482d6512b9992372e9430e
kmadridr <kmadridr@outlook.com>
2018-09-27 19:33:43 -0700

    + Terminado pa la clase

---
+++ src/com/company/Symbols.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Symbols {
+    /**
+     *
+     * Aqui va tar la tabla de simbolos
+     *
+     */
+
+    public static ArrayList<ArrayList<String>>  table;
+
+
+}
